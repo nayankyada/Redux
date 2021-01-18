4faec0833c02e8e404e90cf11e6981352bd8c53a
nayankyada <kayadanayan@gmail.com>
2021-01-18 22:19:55 +0530

    done

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-
+import {connect} from 'react-redux'
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
 
     counterChangedHandler = ( action, value ) => {
         switch ( action ) {
@@ -28,14 +25,26 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubCounter}  />
             </div>
         );
     }
 }
-
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        ctr:state.counter
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return{
+        onIncrementCounter: () => dispatch({type:'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type:'DECREMENT'}),
+        onAddCounter: () => dispatch({type:'ADD',value:5}),
+        onSubCounter: () => dispatch({type:'SUB',value:5}),
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);
